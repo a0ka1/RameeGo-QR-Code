2c976373e24ff4015407d23aec08dca4561e7a94
a0ka1 <support@rameego.com>
2025-04-15 11:45:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const fibAppLink = document.getElementById("fib-app");
+
+  if (fibAppLink) {
+    fibAppLink.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent default link behavior
+
+      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      if (/iPhone|iPad|iPod/i.test(userAgent)) {
+        window.location.href =
+          "https://apps.apple.com/us/app/first-iraqi-bank/id1545549339";
+      } else if (/Android/i.test(userAgent)) {
+        window.location.href =
+          "https://play.google.com/store/apps/details?id=com.firstiraqibank.personal&pcampaignid=web_share";
+      } else {
+        window.location.href = "https://fib.iq"; // Fallback to the website (you can change this if needed)
+      }
+    });
+  }
+});
